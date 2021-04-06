4a8d9f3c5404ed5a1decc4f5ea986a52ea9e897f
Farhan Ali Rahmoon <farhantcs13@gmail.com>
2021-04-06 09:31:55 -0700

    Add black theme

--- ext.js
+++ ext.js
@@ -9,3 +9,21 @@ function toggleMenu() {
     menuToggle.classList.toggle("active");
     menu.classList.toggle("active");
 }
+
+// This is for darkmode
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌗', // default: ''
+    autoMatchOsTheme: false // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
